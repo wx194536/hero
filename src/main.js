e069f296cbcd73de0952ba86ea289c249d4a74c7
xu <810686844@qq.com>
2018-09-02 22:05:18 +0800

    配置首页

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+//基于webpack的开发，是基于模块化的开发
+
+import Vue from 'vue';
+import App from './App.vue';
+
+//导入全局的CSS样式
+import 'bootstrap/dist/css/bootstrap.css';
+//导入CSS样式
+import './assets/index.css';
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
